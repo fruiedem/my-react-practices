2acabaeac59a11d4a2a8a5cff4f6bf39a1cb8310
changjunkim <changjunkim@lotte.net>
2025-01-31 16:51:05 +0900

    객체와 배열 사용 예시

--- first-project/src/App.jsx
+++ first-project/src/App.jsx
@@ -1,5 +1,7 @@
 import Child from './Child';
 import { ClassComp, FuncComp, ArrowFuncComp } from './Component3types';
+import BasicExpressions from './BasicExpressions';
+import ObjectArrayExpressions from './ObjectArrayExpressions';
 function Hello(){
   return <h1>hello</h1>
 }
@@ -11,9 +13,14 @@ function App() {
       <Hello/>
       <h2>world</h2>
       <Child/>
+      <h2>----- 컴포넌트 정의 3가지 -----</h2>
       <ClassComp/>
       <FuncComp/>
       <ArrowFuncComp/>
+      <h2>----- 중괄호 표현식 사용 -----</h2>
+      <BasicExpressions/>
+      <h2>----- 객체와 배열의 사용 -----</h2>
+      <ObjectArrayExpressions/>
     </>
   )
 }
---
+++ first-project/src/ObjectArrayExpressions.jsx
@@ -0,0 +1,25 @@
+function ObjectArrayExpressions(){
+    const user = {
+        name: "Jane",
+        email: "jane@example.com"
+    }
+    const colors = ["red", "blue", "green"];
+    const numbers = [1, 2, 3, 4, 5];
+    return (
+        <div>
+            <p>User: {user.name} {user.email}</p>
+            <p>First color: {colors[0]}</p>
+            <p>Color count: {colors.length}</p>
+
+            <p>Doubleds: {
+                numbers.map(n =>n*2).join(", ")
+            }</p>
+            <p>Evens:{
+                numbers.filter(n=>n%2 === 0).join(", ")
+                }
+            </p>
+        </div>
+    );
+}
+
+export default ObjectArrayExpressions
