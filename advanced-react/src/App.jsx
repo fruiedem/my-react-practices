ea30501e79489b50b07994f7d55ab14c346e5514
changjunkim <changjunkim@lotte.net>
2025-02-01 18:14:01 +0900

    5단계. 컴포넌트 리스트 렌더링을 위한 객체 배열화

--- advanced-react/src/App.jsx
+++ advanced-react/src/App.jsx
@@ -6,6 +6,7 @@ import InfoCard from './InfoCard';
 2단계. props 구조분해할당 - 코드 중복 개선
 3단계. props 기본값 설정
 4단계. props spreading - 코드 가독성 개선
+5단계. 컴포넌트 리스트 렌더링을 위한 객체 배열화
  */
 
 const cardData1 = {
@@ -18,15 +19,25 @@ const cardData2 = {
   title: "React Composition",
   content: "Composition makes your components more reusable"
 };
+
+const cardData = [
+  {
+  idx: 1,
+  title : "Props in React",
+  content : "Porps pass data from one component to another",
+  author : "Alice"
+  },
+  {
+  idx: 2,
+  title: "React Composition",
+  content: "Composition makes your components more reusable",
+  author : "Charlie"
+  }
+]
 function App() {
   return (
     <>
-      <InfoCard
-        {...cardData1}
-      />
-      <InfoCard
-        {...cardData2}
-      />
+      {cardData.map(cardData => <InfoCard key={cardData.idx} {...cardData}/>)}
     </>
   )
 }
