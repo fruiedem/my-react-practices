12ba3d7c19b37926b2035e4dd81f9cf2cfad1c2f
fruiedem <fruiedem@gmail.com>
2025-02-01 22:42:31 +0900

    9단계. 이벤트 핸들러-button 요소의 다양한 이벤트 속성

--- advanced-react/src/Button.jsx
+++ advanced-react/src/Button.jsx
@@ -1,6 +1,21 @@
+const handleEvent = (name, which) => {
+    console.log(name, which);
+}
+
 const Button = ({name}) => (
     <button
-    onClick = {() => console.log(name)}
+        onMouseEnter={
+            () => handleEvent(name, 'MouseEnter')
+        }
+        onMouseLeave={
+            () => handleEvent(name, 'MouseLeave')
+        }
+        onDoubleClick={
+            () => handleEvent(name, 'DoubleClick')
+        }
+        onContextMenu={
+            () => handleEvent(name, 'onContextMenu')
+        }
     >
         {name}
     </button>
