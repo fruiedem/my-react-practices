3d337953f5d145dde01f5b9f05d1b9edc6ca5ed8
changjunkim <changjunkim@lotte.net>
2025-02-01 19:37:37 +0900

    6단계. 함수 Props 전달

--- advanced-react/src/App.jsx
+++ advanced-react/src/App.jsx
@@ -1,12 +1,17 @@
 import './App.css';
 import './Card.module.css';
 import InfoCard from './InfoCard';
+import ProductCard from './ProductCard';
 /*
+InfoCard.jsx
 1단계. InfoCard 컴포넌트 정의 및 props 전달
 2단계. props 구조분해할당 - 코드 중복 개선
 3단계. props 기본값 설정
 4단계. props spreading - 코드 가독성 개선
 5단계. 컴포넌트 리스트 렌더링을 위한 객체 배열화
+
+ProductCard.jsx
+6단계. 함수 Props 전달
  */
 
 const cardData1 = {
@@ -34,10 +39,19 @@ const cardData = [
   author : "Charlie"
   }
 ]
+
+const product = {
+  name: "laptop",
+  price: 123.456
+}
 function App() {
   return (
     <>
       {cardData.map(cardData => <InfoCard key={cardData.idx} {...cardData}/>)}
+      <ProductCard 
+        {...product}
+        formatPrice = {(p) => `$${p.toFixed(2)}`}
+      />
     </>
   )
 }
